6e73115e8aa421675409d78f60784ce402b65d17
Piton <Iapiton@yandex.ru>
2024-03-16 15:43:47 +0300

    fixed

--- impl/src/main/java/ru/skillbox/diplom/group46/social/network/impl/Application.java
+++ impl/src/main/java/ru/skillbox/diplom/group46/social/network/impl/Application.java
@@ -1,12 +1,19 @@
 package ru.skillbox.diplom.group46.social.network.impl;
 
 import org.springframework.boot.SpringApplication;
+
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import ru.skillbox.diplom.group46.social.network.impl.repository.base.BaseRepositoryImpl;
+import ru.skillbox.diplom.group46.social.network.impl.service.geo.GeoClient;
 
 /**
  * Application
@@ -18,7 +25,7 @@ import ru.skillbox.diplom.group46.social.network.impl.repository.base.BaseReposi
 @SpringBootApplication
 @EntityScan("ru.skillbox.diplom.group46.social.network.domain")
 @EnableJpaRepositories(repositoryBaseClass = BaseRepositoryImpl.class)
-@EnableFeignClients(basePackages = "ru.skillbox.diplom.group46.social.network.*" )
+@EnableFeignClients(basePackageClasses = GeoClient.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
