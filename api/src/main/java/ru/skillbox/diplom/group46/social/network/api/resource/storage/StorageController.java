50d9df26747254decddaa0bc99bb1fac3ab0e1f9
bennedi <110489580+bublik2424@users.noreply.github.com>
2024-03-19 19:40:25 +0300

    fix cloudinary

--- api/src/main/java/ru/skillbox/diplom/group46/social/network/api/resource/storage/StorageController.java
+++ api/src/main/java/ru/skillbox/diplom/group46/social/network/api/resource/storage/StorageController.java
@@ -1,17 +1,15 @@
 package ru.skillbox.diplom.group46.social.network.api.resource.storage;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-@RequestMapping("/api/v1/upload")
+@RequestMapping("/api/v1")
 public interface StorageController {
+    @PostMapping("/storage")
     String uploadFile(@RequestParam("image") MultipartFile multipartFile) throws IOException;
+
 }
--- impl/src/main/java/ru/skillbox/diplom/group46/social/network/impl/resource/storage/StorageControllerImpl.java
+++ impl/src/main/java/ru/skillbox/diplom/group46/social/network/impl/resource/storage/StorageControllerImpl.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import ru.skillbox.diplom.group46.social.network.api.resource.storage.StorageController;
+import ru.skillbox.diplom.group46.social.network.impl.service.storage.StorageService;
 
 import java.io.IOException;
 
@@ -12,8 +13,9 @@ import java.io.IOException;
 @RestController
 @RequiredArgsConstructor
 public class StorageControllerImpl implements StorageController {
+    private final StorageService fileUpload;
     @Override
     public String uploadFile(MultipartFile multipartFile) throws IOException {
-        return null;
+        return fileUpload.uploadFile(multipartFile);
     }
 }
